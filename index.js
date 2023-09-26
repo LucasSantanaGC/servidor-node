c38b151b2e6a7de79ad54e1c395571aed8ca8643
LucasSantanaGC <124914962+LucasSantanaGC@users.noreply.github.com>
2023-09-26 10:16:34 -0300

    a

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const http = require('http')
 
+
+
 const server = http.createServer((req, res) => {
-  console.log(req.method)
-  console.log(req.url)
-  res.statusCode = 200
-res.end('<h1>Servidor conectado com sucesso Filipão sênior!!!</h1>')
-  //   res.end(console.log('CONECTADO NODE HTTP'))
+
+    console.log(req.method)
+
+    console.log(req.url)
+
+    res.statusCode = 200
+
+    res.end('<h1>Servidor conecnodetado com sucesso Filipão sênior!!!</h1>')
+
+    //   res.end(console.log('CONECTADO NODE HTTP'))
+
 });
 
-server.listen(80, () => {
-  console.log('servidor ativo LUCAS!')
+
+
+server.listen(3000, () => {
+
+    console.log('servidor ativo LUCAS!')
+
 })
+
+
 
 console.log("teste 2")
