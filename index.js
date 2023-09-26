26ec1dea7d0fff42ec0a0e040e05212dd998d4d0
LucasSantanaGC <124914962+LucasSantanaGC@users.noreply.github.com>
2023-09-26 09:52:23 -0300

    porta

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const server = http.createServer((req, res) => {
   console.log(req.method)
   console.log(req.url)
   res.statusCode = 200
-res.end('<h1>Servidor conectado com sucesso Lucas !!!</h1>')
+res.end('<h1>Servidor conectado com sucesso Filipão sênior!!!</h1>')
   //   res.end(console.log('CONECTADO NODE HTTP'))
 });
 
-server.listen(3000, () => {
+server.listen(80, () => {
   console.log('servidor ativo LUCAS!')
 })
 
